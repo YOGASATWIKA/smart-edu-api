680496bdb52163b26e2bce4c9b2c1cdc73634642
YOGASATWIKA <satwikayoga3@gmail.com>
2025-10-05 12:03:17 +0800

    :bug: fix: model custome

--- repository/model_repository.go
+++ repository/model_repository.go
@@ -1,12 +1,14 @@
 package repository
 
 import (
+	"context"
 	"smart-edu-api/config"
-	"smart-edu-api/data/model/response"
 	"smart-edu-api/entity"
 	"smart-edu-api/helper"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
@@ -21,12 +23,30 @@ func CreateModel(model entity.Model) (entity.Model, error) {
 	return model, nil
 }
 
-func GetAllModel() ([]response.ModelResponse, error) {
+func GetOutlineModel() ([]entity.Model, error) {
 	client := config.GetMongoClient()
 	collection := client.Database("smart_edu").Collection("models")
 
-	var result entity.Model
-	return result.GetAll(collection)
+	var results []entity.Model
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{
+		"status": bson.M{"$ne": "DELETED"},
+	}
+	findOptions := options.Find()
+	findOptions.SetSort(bson.M{"updated_at": -1})
+
+	cursor, err := collection.Find(ctx, filter, findOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if err := cursor.All(ctx, &results); err != nil {
+		return nil, err
+	}
+	return results, nil
 }
 
 func GetModelById(id string) (*entity.Model, error) {

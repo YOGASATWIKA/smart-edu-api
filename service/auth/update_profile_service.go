2c2cc86bd951c345c548025aaab5647b7ececc79
YOGASATWIKA <satwikayoga3@gmail.com>
2025-11-20 13:30:26 +0800

    :bug: fix: update profile

--- service/auth/update_profile_service.go
+++ service/auth/update_profile_service.go
@@ -1,17 +1,12 @@
 package auth
 
 import (
-	"context"
-	"smart-edu-api/config"
 	request "smart-edu-api/data/user/request"
-	"smart-edu-api/entity"
 	"smart-edu-api/repository"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func HandleUpdateProfile(c *fiber.Ctx) error {
@@ -37,19 +32,6 @@ func HandleUpdateProfile(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(map[string]any{
 			"message": err.Error(),
 		})
-	}
-
-	client := config.GetMongoClient()
-	collection := client.Database("smart_edu").Collection("users")
-
-	err = collection.FindOne(context.TODO(), bson.M{"email": request.Email}).Decode(&entity.User{})
-	if err == nil {
-		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-			"message": "Email Sudah Terdaftar",
-		})
-	}
-	if err != mongo.ErrNoDocuments {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Database error"})
 	}
 
 	existing.Name = request.Name
